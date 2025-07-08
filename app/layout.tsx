b386ac0720800ed802f2978257957918424c687b
Lakshan Ganesan <lakshanganesan@Lakshans-MacBook-Air.local>
2025-07-08 22:30:02 +0530

    dark mode added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,42 +1,25 @@
 import type { Metadata } from 'next'
 import './globals.css'
-<<<<<<< HEAD
-
-export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-=======
 import { ThemeProvider } from '@/components/theme-provider'
 
 export const metadata: Metadata = {
   title: 'Wave3',
-  description: 'Landing page for Wave3',
->>>>>>> 9c33c70 (dark mode changes)
+  description: 'Created with v0',
   generator: 'v0.dev',
 }
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode
-}>) {
+}) {
   return (
-<<<<<<< HEAD
-    <html lang="en">
-      <body>{children}</body>
-=======
     <html lang="en" suppressHydrationWarning>
       <body>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="light"
-          enableSystem
-          disableTransitionOnChange
-        >
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
         </ThemeProvider>
       </body>
->>>>>>> 9c33c70 (dark mode changes)
     </html>
   )
 }
